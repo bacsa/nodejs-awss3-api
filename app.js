71e3fa48bc2bc34c53aef7cbbb8ffa362a10f2e2
Bihari Csaba <bihari.csaba@live.com>
2019-05-21 12:08:27 +0200

    Mocha - Chai test environment configured

--- app.js
+++ app.js
@@ -5,50 +5,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // Logger
 var logger = require('morgan');
-// DB connections
-const mongoConnection = require('./db/mongodb.connection');
 // Routes
 const apiroutes = require('./routes/api.route');
-const frontendroutes = require('./routes/frontend.route');
-const productroutes = require('./routes/product.route');
-
 
 const app = express();
-app.use(logger('dev'));
+//don't show the log when it is test
+if(config.environment !== 'testing') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
-app.set('view engine', 'ejs');
-
-app.use('/', frontendroutes);
 app.use('/api', apiroutes);
-app.use('/products', productroutes);
-
-/*
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
- 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-*/
-
-
 
 app.listen(config.port, () => {
     console.log('Server is up and running on port ' + config.port);
 });
+
+module.exports = app; // for testing
---
+++ test/testing-awss3.js
@@ -0,0 +1,59 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'testing';
+// Config
+const config = require('../config/config');
+
+const router = require('express').Router();
+
+var awss3Controller = require('../controllers/awss3.controller');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Buckets', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        chai.request(config.api_url + 's3')
+            .delete('/bucket/teszteles-111')
+            .end((err, res) => {
+                  res.should.have.status(200);
+              done();
+            }); 
+    });
+/*
+  * Test the /GET route
+  */
+  describe('/GET Buckets', () => {
+      it('it should GET all the buckets', (done) => {
+        chai.request(config.api_url + 's3')
+            .get('/')
+            .end((err, res) => {
+                  res.should.have.status(200);
+              done();
+            });
+      });
+  });
+  /*
+  * Test the /POST route
+  */
+  describe('/POST Create Bucket', () => {
+      it('it should create a new Bucket', (done) => {
+          let bucket = {
+              bucketname: "teszteles-111"
+          }
+        chai.request(config.api_url + 's3')
+            .post('/')
+            .send(bucket)
+            .end((err, res) => {
+                  res.should.have.status(200);
+              done();
+            });
+      });
+
+  });
+
+});
